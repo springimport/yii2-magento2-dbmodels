eeaab86a7c291bfe234ad7622821439e83a2294a
SpringImport <contact@springimport.com>
2016-08-08 11:02:11 -0400

    Revert "added const of addresses"
    This reverts commit 576b577cf540a3f179bf57a2f1db002ec479f78a.

--- src/models/SalesOrderAddress.php
+++ src/models/SalesOrderAddress.php
@@ -38,8 +38,6 @@ use Yii;
  */
 class SalesOrderAddress extends \yii\db\ActiveRecord
 {
-    const ADDRESS_BILLING_TYPE  = 'billing';
-    const ADDRESS_SHIPPING_TYPE = 'shipping';
 
     /**
      * @inheritdoc
